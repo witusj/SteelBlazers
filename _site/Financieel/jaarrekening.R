b1341a71977ce116bbfb3e2c1adf6f9d2a068fde
witusj <wfth69@gmail.com>
2016-10-31 23:42:39 +0100

    Added tabs

---
+++ _site/Financieel/jaarrekening.R
@@ -0,0 +1,23 @@
+library(gsheet)
+library(googleVis)
+library(dplyr)
+
+frmJaarrekening <- gsheet2tbl(
+  'https://docs.google.com/spreadsheets/d/1KB9PlhP7K4ghBpZsdzUBiQgsUw97SZVnP4hRiZT927M/pub?output=csv'
+)
+colnames(frmJaarrekening) <- NULL
+frmJaarrekening[,c(2,4)] <- format(frmJaarrekening[,c(2,4)], decimal.mark=",",big.mark=".",small.mark=".")
+frmJaarrekening[is.na(frmJaarrekening)] <-NULL
+tblJaarrekening <- htmlTable(frmJaarrekening,
+                             cgroup=c("Activa", "Passiva"),
+                             n.cgroup=c(2,2),
+                             caption="Balans per 31.12.2015 (in EUR)",
+                             align="lrlr",
+                             rnames=FALSE,
+                             css.cell = c("padding-left: .5em; padding-right: .8em;",
+                                          "padding-left: 1.8em; padding-right: .8em;",
+                                          "padding-left: .5em; padding-right: .8em;",
+                                          "padding-left: 1.8em; padding-right: .8em;"),
+                             col.columns = c("none", "#F1F0FA"),
+                             total=TRUE)
+tblJaarrekening <- gsub("NA</td>", "", tblJaarrekening)
